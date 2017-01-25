0a5c7ffe7bcf4cc258a3c94af1ac6e5737997795
Uriel Loredo <urieloredo@gmail.com>
2017-01-24 18:19:20 -0600

    [ADD]Clone function

--- _markup/assets/js/app.js
+++ _markup/assets/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     cloneBranchOne = function (){
         var e = $('.sakuraTreeFlower:first-child');
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 5; i++) {
         e.clone().insertAfter(e);
         }
     };
